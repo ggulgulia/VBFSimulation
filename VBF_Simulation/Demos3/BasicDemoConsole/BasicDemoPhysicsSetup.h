6c3c91fb618cd20db335450cf3f131e41ab5aaa5
gajendra <gajendra.gulgulia@tum.de>
2018-11-19 11:30:33 +0100

    Upgraded demos + my test sim for bullet physics
    1. Upgraded files:
        * /VBF_Simulation/Demos3/BasicDemo/ : dependency on local
        * /VBF_Simulation/SimFiles
    2. New Demos
        * VBF_Simulation/Demos3/BasicDemoConsole/
        * VBF_Simulation/Demos3/BasicDemoCustomOpenGL2/

---
+++ VBF_Simulation/Demos3/BasicDemoConsole/BasicDemoPhysicsSetup.h
@@ -0,0 +1,28 @@
+#ifndef BASIC_DEMO_PHYSICS_SETUP_H
+#define BASIC_DEMO_PHYSICS_SETUP_H
+
+class btRigidBody;
+class btCollisionShape;
+class btBroadphaseInterface;
+class btConstraintSolver;
+class btCollisionDispatcher;
+class btDefaultCollisionConfiguration;
+class btDiscreteDynamicsWorld;
+class btTransform;
+class btVector3;
+class btBoxShape;
+#include <bullet/LinearMath/btVector3.h>
+
+#include <bullet/LinearMath/btAlignedObjectArray.h>
+
+
+#include "../../Demos3/btgui/Bullet3AppSupport/CommonRigidBodySetup.h"
+
+struct BasicDemoPhysicsSetup : public CommonRigidBodySetup
+{
+	
+	virtual void initPhysics(GraphicsPhysicsBridge& gfxBridge);
+
+};
+
+#endif //BASIC_DEMO_PHYSICS_SETUP_H
