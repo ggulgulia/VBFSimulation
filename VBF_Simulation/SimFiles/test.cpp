af07cb97b2fc2b1e7aa7a5e2bd6e58dce982566a
gajendra <gajendra.gulgulia@tum.de>
2018-12-08 17:21:34 +0100

    World Objcet under VBF namespace
    1. beginning of object orientation of the typical objects needed in vbf
    simulation
    2. introduction of namespace VBF (stands for vibratory bowl feeder)
    3. corresponding modifications in test.cpp and CMakeLists.txt files

--- VBF_Simulation/SimFiles/test.cpp
+++ VBF_Simulation/SimFiles/test.cpp
@@ -1,8 +1,5 @@
-#include <bullet/btBulletDynamicsCommon.h>
-#include <bullet/LinearMath/btIDebugDraw.h>
-#include <iostream>
-#include <vector>
 #include "helper.hpp"
+#include "vbf_world.hpp"
 
 //overloading the priting operator
 std::ostream operator<<(std::ostream &out, const btTransform &trans){
@@ -32,24 +29,16 @@ int main(int argc, char *argv[])
 
     std::cout << "attempt to run hello world like program using modern c++ and with GUI debugDraw\n";
 
-    //create the properties of the world
-    btDefaultCollisionConfiguration* collisonConfig = new btDefaultCollisionConfiguration();
-    btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisonConfig);
-    btBroadphaseInterface* interface = new btDbvtBroadphase();
-    btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
-    
-    // initialize the world with the above properties
-    btDiscreteDynamicsWorld* world = new btDiscreteDynamicsWorld(dispatcher, interface, solver, collisonConfig);
-    world->setGravity(btVector3(0.0, -9.81, 0));
-    
+    //create world for vbf simulation
+    VBF::World* vbf_world = new VBF::World();
+    vbf_world->intialize_new_world();
+   
     //create OpenGL Window
-
     b3gDefaultOpenGLWindow* window = new b3gDefaultOpenGLWindow();
     b3gWindowConstructionInfo wci;
     wci.m_openglVersion = 2;
     wci.m_width = sWidth;
     wci.m_height = sHeight;
-
     window->createWindow(wci);
     window->setResizeCallback(MyResizeCallback);
     window->setMouseButtonCallback(MyMouseButtonCallback);
@@ -60,8 +49,8 @@ int main(int argc, char *argv[])
     //initialize visualization framework
     GLDebugDrawer *debugDraw = new GLDebugDrawer;
     debugDraw->setDebugMode(1); //what does 1 mean and what are the other options?
-    world->setDebugDrawer(debugDraw); 
-    
+    vbf_world->set_debug_drawer(debugDraw); 
+
    
     //create placeholder for shapes, some kind of array
     std::vector<btCollisionShape*> collisionShapes;
@@ -105,30 +94,17 @@ int main(int argc, char *argv[])
         rbInfo.push_back(btRbConstrInfo(mass[i], myMotionstate[i], collisionShapes[i], inertia[i]));
         rbody.push_back(new btRigidBody(rbInfo[i]));
 
-        world->addRigidBody(rbody[i]);
     }
 
-    //do some simulation
+    vbf_world->add_rigid_bodies_to_world(rbody);
+
+    ////do some simulation
     for (size_t i = 0; i < 100000; ++i) {
-        world->stepSimulation(1.f/60.f, 10);
-
-        //print positions of all objects
-        for (int j = 0; j<world->getNumCollisionObjects(); ++j){
-
-            btCollisionObject *obj = world->getCollisionObjectArray()[j];
-            btRigidBody* bdy = btRigidBody::upcast(obj);
-            
-            if(bdy && bdy->getMotionState()){
-                btTransform trans;
-                bdy->getMotionState()->getWorldTransform(trans);
-                world->debugDrawWorld();
-                std::cout << "world pos: " << trans.getOrigin().getX() <<"\nY: " << trans.getOrigin().getY() << "\nZ:" << trans.getOrigin().getZ() << "\n";
-            }
-        }
-        
+        vbf_world->step_simulation(1.0/60.0, 10.0);
+        vbf_world->print_updated_positions();
     }
 
-    releaseResources(collisionShapes, rbody, myMotionstate);
+    //releaseResources(collisionShapes, rbody, myMotionstate);
 
     return 0;
 }
