0dab4cb2348193aaaf46badfc1c602b9b43841d4
gajendra <gajendra.gulgulia@tum.de>
2018-12-09 16:08:33 +0100

    some more functionalities in the classes

--- VBF_Simulation/SimFiles/VBF_RigidBodies.hpp
+++ VBF_Simulation/SimFiles/VBF_RigidBodies.hpp
@@ -10,7 +10,6 @@ typedef btCollisionShape VBF_Shape;
 
 namespace VBF{
 
-
     class RigidBody{
 
         private:
@@ -21,7 +20,6 @@ namespace VBF{
             btVector3 m_inertia;
             size_t m_index; //might be helpful for book keeping 
                             //when there are many rigid bodies
-        protected:
             btRigidBody *m_rbody;
 
         public:
@@ -32,10 +30,43 @@ namespace VBF{
             //user constructor
             explicit RigidBody(std::string name, VBF_Shape* shape, btVector3 origin,
                                double mass, btVector3 inertia, size_t index);
+            
+            //copy Constructor
+            RigidBody(const RigidBody& vbf_rb):
+            m_name(vbf_rb.get_name()),
+            m_shape(vbf_rb.get_shape()),
+            m_origin(vbf_rb.get_origin()), 
+            m_mass(vbf_rb.get_mass()),
+            m_inertia(vbf_rb.get_inertia()), 
+            m_index(vbf_rb.get_index()+1),
+            m_rbody(vbf_rb.get_rbody())
+            { //empty copy constructor body
+             }
+            
+            //constructor that simply creates a copy of RigidBody 
+            //by transforming its position (and orientation)   
+            RigidBody(const RigidBody& rbody, btVector3 origin):
+            RigidBody(rbody) //call the copy constructor
+            {
+                m_origin = origin; //
+                btTransform shapeTransform;
+                shapeTransform.setIdentity();
+                shapeTransform.setOrigin(m_origin);
+                MotionState *motionState = new MotionState(shapeTransform);
+                btRbConstrInfo rbinfo(m_mass, motionState, m_shape, m_inertia);
+                m_rbody = new btRigidBody(rbinfo);
+            }
+            
 
-            btRigidBody* get_rbody();
-            virtual std::string get_name();
+            virtual btRigidBody* get_rbody() const;
+            virtual std::string get_name()   const;
+            virtual VBF_Shape* get_shape()   const { return m_shape;  } 
+            virtual btVector3 get_origin()   const { return m_origin; }
+            virtual double get_mass()        const { return m_mass;   }
+            virtual btVector3 get_inertia()  const { return m_inertia;}
+            virtual size_t get_index()       const { return m_index;  }
     };
+    
     class Cube : public RigidBody{
 
         private:
@@ -44,9 +75,15 @@ namespace VBF{
         public:
 
             explicit Cube(double length, btVector3 origin, btVector3 inertia, double mass, size_t index);
+            
+            Cube(const Cube& cube, btVector3 origin):
+                       m_length(get_length()),
+                       RigidBody(cube, origin)
+           { }
 
             ~Cube();
             virtual std::string get_name();
+            double get_length() const { return m_length;}
     };
 
     class Sphere: public RigidBody{
@@ -57,9 +94,16 @@ namespace VBF{
         public:
 
             explicit Sphere(double radius, btVector3 origin, btVector3 inertia, double mass, size_t index);
+            
+            Sphere(const Sphere& sphere, btVector3 origin):
+                    m_radius(get_radius()),
+                    RigidBody(sphere, origin)
+            { }
+             
             ~Sphere();
 
             virtual std::string get_name();
+            double get_radius() const { return m_radius;}
     };
 
 }//end of name space
