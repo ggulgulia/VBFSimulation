95ec65141e5f72e53942453272a368ef82b1259f
gajendra <gajendra.gulgulia@tum.de>
2019-01-13 12:27:37 +0100

    program importing stl geometry
    file imports STL geometry and returns an inbuilt
    GLInstancingGraphicsShape object.
    
    TODO:
        1. redefine the classes and objects to use it suitably with the
           VBF Simulation and other custom objects defined earlier
        2. integrate graphics (keyboard controllers unnecessary)
        3. multiple instances of same object and input location
    
        List of Modified/Deleted Files
    	modified:   CMakeLists.txt
    	deleted:    GraphicsApp.cpp
    	deleted:    GraphicsApp.hpp
    	deleted:    GraphicsAppHelper.hpp
    	modified:   ImportSTLSetup.cpp
    	modified:   ImportSTLSetup.hpp
    	modified:   LoadMeshFromSTL.hpp

--- VBF_Simulation/Demos3/ImportSTLDemo/ImportSTLSetup.cpp
+++ VBF_Simulation/Demos3/ImportSTLDemo/ImportSTLSetup.cpp
@@ -5,6 +5,7 @@
 #include <OpenGLWindow/GLInstanceGraphicsShape.h>
 #include <bullet/btBulletDynamicsCommon.h>
 #include <OpenGLWindow/SimpleOpenGL3App.h>
+#include "VBF_GraphicsBridge.hpp"
 
 
 
@@ -12,17 +13,10 @@ ImportSTLSetup::ImportSTLSetup(const std::string &fileName,
                               int width, 
                               int height):
                               m_filename(fileName)
-                              //m_app(new GraphicsApp(fileName.c_str(),width, height))
 {
 
 }
 
-
-ImportSTLSetup::ImportSTLSetup(GraphicsApp* app)
-:m_app(app)
-{
-    
-}
 
 ImportSTLSetup::~ImportSTLSetup()
 {
@@ -39,34 +33,16 @@ void ImportSTLSetup::initPhysics()
     std::fstream file;
 	int prefixIndex=-1;
     int numPrefixes = 2;
-	for (int i=0;i<numPrefixes;i++)
-	{
-		//f = fopen(relativeFileName,"r");
-        file.open(m_filename);
-		if (file.is_open())
-		{
-			//fclose(f);
-            file.close();
-			prefixIndex = i;
-			break;
-		}
-	}
-	
-	
-	if (prefixIndex<0)
-		return;
 	
 	btVector3 shift(0,0,0);
 	btVector3 scaling(10,10,10);
-	GLInstanceGraphicsShape* gfxShape = LoadMeshFromSTL(m_filename.c_str());
+	GLInstanceGraphicsShape* gfxShape = LoadMeshFromSTL(m_filename);
 	btTransform trans;
 	trans.setIdentity();
 	trans.setRotation(btQuaternion(btVector3(1,0,0),SIMD_HALF_PI));
 	btVector3 position = trans.getOrigin();
 	btQuaternion orn = trans.getRotation();
 	btVector3 color(0,0,1);
-	int shapeId = m_app->get_renderer_interface()->registerShape(&gfxShape->m_vertices->at(0).xyzw[0], gfxShape->m_numvertices, &gfxShape->m_indices->at(0), gfxShape->m_numIndices);
-	m_app->get_renderer_interface()->registerGraphicsInstance(shapeId,position,orn,color,scaling);
 
 	
 }
@@ -76,6 +52,9 @@ int main(int argc, char *argv[]){
     VBF::World* vbf_world = new VBF::World();
     vbf_world->initialize_new_world();
     std::string fileName("l_finger_tip.stl");
+    VBF::Window* vbf_window = new VBF::Window(vbf_world, 800, 600, "Hello VBF World");
+    //vbf_window->create_window();
+
     ImportSTLSetup* stl_body = new ImportSTLSetup(fileName);    
     stl_body->initPhysics();
 
