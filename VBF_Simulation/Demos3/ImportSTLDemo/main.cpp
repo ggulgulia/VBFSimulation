d5c00dda902437edcfcc64f4ba974db97d147ce4
gajendra <gajendra.gulgulia@tum.de>
2019-01-19 17:09:11 +0100

    Importing mesh with graphics
    The imported STL files can be visualized in the
    graphics bridge that was created earlier (code reuse)
    maintaining similar workflow.
    
    Some of the old files from bullet have been gotten rid of
    
    TODO:
        1. fine grain control over the input position of the part
        2. how to set the orientation of the imported mesh
        3. align camera position relative to the position imported geometry
        4. scaling up and down the object
    
     Changes to be committed:
    	modified:   CMakeLists.txt
    	deleted:    CommonGraphicsApp.h
    	deleted:    CommonPhysicsSetup.h
    	deleted:    CommonRigidBodySetup.h
    	modified:   ImportSTLSetup.cpp
    	modified:   ImportSTLSetup.hpp
    	renamed:    MeshFiles/StufeFein%150x30x10_0.stl -> MeshFiles/StufeFein150x30x100.stl
    	new file:   VBF_CommonPhysics.cpp
    	new file:   VBF_CommonPhysics.hpp
    	new file:   VBF_Cube.cpp
    	new file:   VBF_Cube.hpp
    	new file:   VBF_RigidBodies.cpp
    	new file:   VBF_RigidBodies.hpp
    	modified:   main.cpp

--- VBF_Simulation/Demos3/ImportSTLDemo/main.cpp
+++ VBF_Simulation/Demos3/ImportSTLDemo/main.cpp
@@ -1,16 +1,66 @@
 #include "ImportSTLSetup.hpp"
 #include "VBF_World.hpp"
 #include "VBF_GraphicsBridge.hpp"
-#include <OpenGLWindow/SimpleOpenGL3App.h>
+#include "VBF_Cube.hpp"
+#include "VBF_CommonPhysics.hpp"
+
+void get_ground(VBF::RigidBody*& ground){
+
+        //create a ground
+        double grLength = 400; 
+        btVector3 grOrigin = btVector3(0.0, -400.0, 0.0);
+        btVector3 grInertia = btVector3(0.0, 0.0, 0.0);
+        double grMass = 0.0; //ground is static object, doesn't interact
+        size_t grIndex = 23; 
+        ground = new VBF::Cube(grLength, grOrigin, grInertia, grMass, grIndex);
+}
+
 
 int main(int argc, char *argv[]){
-
+    
+    //create a world for vbf simulation
     VBF::World* vbf_world = new VBF::World();
     vbf_world->initialize_new_world();
-    std::string fileName("l_finger_tip.stl");
+    
+    //create a ground reference
+    VBF::RigidBody *ground;
+    get_ground(ground);
+
+     //import the stl file    
+    //std::string fileName("l_finger_tip.stl");
+    std::string fileName("StufeFein150x30x100.stl");
+    VBF::ImportSTLSetup* stl_body = new VBF::ImportSTLSetup(fileName); 
+    
+     //create physics
+    VBF::CommonPhysics phy(vbf_world, ground, stl_body->get_vbf_rbody());
+    phy.initPhysics();
+    
+    //vis bridge
     VBF::Window* vbf_window = new VBF::Window(vbf_world, 800, 600, "Hello VBF World");
-    //vbf_window->create_window();
-    VBF::ImportSTLSetup* stl_body = new VBF::ImportSTLSetup(fileName);    
+    vbf_window->create_window();VBF_Vis vis_bridge;
+    vis_bridge.setDynamicsWorld(vbf_world->get_world());
+    vis_bridge.reshape(800, 600);
+    vis_bridge.setShadows(true);
+    gApp = &vis_bridge;
+
+    //run simulations
+    btClock timer;
+    size_t prevTime = timer.getTimeMicroseconds();
+
+    do{
+        unsigned long currTime = timer.getTimeMicroseconds();
+        if(!vis_bridge.isIdle()){
+             phy.stepSimulation((currTime - prevTime)/1000.);
+        }
+
+        prevTime = currTime;
+        vbf_window->start_rendering();
+        vis_bridge.renderme();
+        phy.debugDraw(2);
+        vbf_window->end_rendering();
+    }while(!vbf_window->requested_exit());
+
+    vbf_window->close_window();
 
     return 0;
 }
