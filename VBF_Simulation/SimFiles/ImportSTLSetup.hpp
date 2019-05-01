98fcaa75cf37af44c9fce6b79cb5c31ad1fb6a7d
gajendra <gajendra.gulgulia@tum.de>
2019-05-01 14:52:43 +0200

    dynamics initial steps
        1. scaling the imported object unifromly along all axes
        2. relatively cleaner code (changed for loops)

---
+++ VBF_Simulation/SimFiles/ImportSTLSetup.hpp
@@ -0,0 +1,32 @@
+#ifndef IMPORT_STL_SETUP_H
+#define IMPORT_STL_SETUP_H
+
+
+#include <string>
+#include <OpenGLWindow/GLInstanceGraphicsShape.h>
+#include <bullet/btBulletDynamicsCommon.h>
+#include "VBF_World.hpp"
+#include "VBF_RigidBodies.hpp"
+
+typedef GLInstanceGraphicsShape VBF_Mesh;
+
+namespace VBF{
+
+    class ImportSTLSetup{
+    
+        private:
+            VBF_Mesh*  m_mesh;
+            std::string m_filename;
+            VBF::RigidBody* m_VBF_rbody;
+            btVector3 m_origin;
+            double m_scale;
+        public:
+            ImportSTLSetup(const std::string &fileName, int width=640, int height=480, btVector3 origin = btVector3(0.0, 0.0, 0.0), double scaling_factor=0.1);
+            virtual ~ImportSTLSetup();
+            virtual VBF_Mesh* get_mesh() const;
+            virtual std::string get_file_name() const;
+            virtual btVector3 get_mesh_origin() const;
+            virtual VBF::RigidBody* get_vbf_rbody() const;
+    };
+}
+#endif //IMPORT_STL_SETUP_H
