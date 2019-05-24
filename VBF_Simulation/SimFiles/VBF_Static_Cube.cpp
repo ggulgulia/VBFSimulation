3a74631e53347655e60c832af2f70a9eb320240c
gajendra <gajendra.gulgulia@tum.de>
2019-05-24 10:35:04 +0200

    Static Cube
    Initial code refactoring to get the static cube inheriting from base
    class Rigid Body.
    
    TODO: create a super class Static Body and inherit the Static Cube and
    sphere from this super class. Repeat for all objects of type Kinematic
    and Dynamic body

---
+++ VBF_Simulation/SimFiles/VBF_Static_Cube.cpp
@@ -0,0 +1,26 @@
+#include "VBF_Static_Cube.hpp"
+
+
+
+//Static_Cube Class Definitions
+VBF::Static_Cube::Static_Cube(double length, btVector3 origin, size_t index):
+               RigidBody(std::string("Static_Cube"),
+               new btBoxShape(btVector3(length, length, length)),
+               origin, index),
+               m_length(length){
+                        
+                MotionState* moSt = RigidBody::get_motion_state();
+                CollShape* shape = RigidBody::get_shape();
+                double mass2 = 0.0;
+                btVector3 inertia2(btVector3(0.0, 0.0, 0.0));
+                btRbConstrInfo rbinfo(mass2, moSt, shape, inertia2);
+                btRigidBody* rbody = new btRigidBody(rbinfo);
+                RigidBody::set_rbody(rbody);
+                //empty constructor body
+               }
+
+//destructor
+VBF::Static_Cube::~Static_Cube(){};
+
+std::string VBF::Static_Cube::get_name() const { return "Static_Cube"; }
+
--- VBF_Simulation/SimFiles/main.cpp
+++ VBF_Simulation/SimFiles/main.cpp
@@ -1,10 +1,7 @@
-#include "ImportSTLSetup.hpp"
 #include "VBF_World.hpp"
 #include "VBF_CommonPhysics.hpp"
 #include "VBF_GraphicsBridge.hpp"
-#include "VBF_Cube.hpp"
-#include "VBF_Sphere.hpp"
-#include "VBF_InitializeSim.hpp"
+#include "VBF_Static_Cube.hpp"
 
 void releaseResources(std::vector<btCollisionShape*> &collShape, std::vector<btRigidBody*> &rbody,
                       std::vector<btDefaultMotionState*> &motionState){
@@ -24,92 +21,84 @@ void releaseResources(std::vector<btCollisionShape*> &collShape, std::vector<btR
     std::cout << "Successfully freed the memory\n";
 }
 
-VBF::Cube* get_ground(){
+VBF::Static_Cube* get_ground(){
 
     //create a ground
     double grLength = 50;
     btVector3 grOrigin = btVector3(0.0, -4-grLength, 0.0);
-    btTransform shapeTrans;
-    shapeTrans.setIdentity();
-    btVector3 grInertia = btVector3(0.0, 0.0, 0.0);
-    double grMass = 0.0; //ground is static object, doesn't interact
     size_t grIndex = 23;
     
-    return new VBF::Cube(grLength, grOrigin, shapeTrans, grInertia, grMass, grIndex);
+    return new VBF::Static_Cube(grLength, grOrigin, grIndex);
  
 }
 
-void get_cubes(std::vector<VBF::RigidBody*>& rigid_bodies_vector){
-
-    //create more objects cube objects
-    std::vector<VBF::RigidBody*> rigid_bodies;
-    double cubeLen = 1.0;
-    double cubeMass = 1.0; //  these are dynamic objects
-    size_t cubeIndex = 12;
-    btVector3 cubeInertia= btVector3(0.0, 0.0, 0.0);
-    size_t array_size = 5;
-    btTransform shapeTrans;
-    shapeTrans.setIdentity();
-
-    for(size_t k=0; k<array_size; ++k){
-        for (size_t i = 0; i < array_size; ++i) {
-           for (size_t j = 0; j < array_size; ++j) {
-               
-               btVector3 cubeOrigin = btVector3(2.0*i, 20+2.0*k, 2.0*j);
-               cubeIndex += j + array_size*i + array_size*k;
-               VBF::Cube *cube = new VBF::Cube(cubeLen, cubeOrigin, shapeTrans, cubeInertia, cubeMass, cubeIndex);
-               rigid_bodies_vector.push_back(cube);
-           } 
-        }
-    }
-}
-
-void get_spheres(std::vector<VBF::RigidBody*>& sphere_vector){
-    
-    double sphereRad = 1.0;
-    double sphereMass = 1.0;
-    size_t sphereIndex = 44;
-    btVector3 sphereInertia = btVector3(0.0, 0.0, 0.0);
-    btTransform shapeTrans;
-    shapeTrans.setIdentity();
-    size_t array_size = 5;
-       for(size_t k=0; k<array_size; ++k){ 
-           for (size_t i = 0; i < array_size; ++i) {
-               for (size_t j = 0; j < array_size; ++j) {
-                   
-                   btVector3 sphereOrigin = btVector3(2.0*i, 20+2.0*k, 2.0*j);
-                   sphereIndex += j + array_size*i + array_size*k;
-                   VBF::Sphere *sph = new VBF::Sphere(sphereRad, sphereOrigin, shapeTrans, sphereInertia, sphereMass, sphereIndex);
-                   sphere_vector.push_back(sph);
-               } 
-            }
-        }
-
-}
-
-
-//this method adds a reference sphere at the axis of  the vbf component for visual check and 
-//debuggging 
-void get_sphere(std::vector<VBF::RigidBody*>& sphere_vector, btVector3 &meshAxis, const double scale){
-    
-    double sphereRad = 1.0;
-    size_t sphereIndex = 44;
-    meshAxis[0] *= scale; meshAxis[1] *= scale; meshAxis[2] *= scale;
-    btVector3 sphereInertia = btVector3(0.0, 0.0, 0.0);
-    btTransform shapeTrans;
-    shapeTrans.setIdentity();
-    VBF::Sphere *sph = new VBF::Sphere(sphereRad, meshAxis, shapeTrans, sphereInertia, 0.0, sphereIndex);
-    sphere_vector.push_back(sph);
-
-}
+//void get_cubes(std::vector<VBF::RigidBody*>& rigid_bodies_vector){
+//
+//    //create more objects cube objects
+//    std::vector<VBF::RigidBody*> rigid_bodies;
+//    double cubeLen = 1.0;
+//    double cubeMass = 1.0; //  these are dynamic objects
+//    size_t cubeIndex = 12;
+//    btVector3 cubeInertia= btVector3(0.0, 0.0, 0.0);
+//    size_t array_size = 5;
+//    btTransform shapeTrans;
+//    shapeTrans.setIdentity();
+//
+//    for(size_t k=0; k<array_size; ++k){
+//        for (size_t i = 0; i < array_size; ++i) {
+//           for (size_t j = 0; j < array_size; ++j) {
+//               
+//               btVector3 cubeOrigin = btVector3(2.0*i, 20+2.0*k, 2.0*j);
+//               cubeIndex += j + array_size*i + array_size*k;
+//               VBF::Static_Cube *cube = new VBF::Static_Cube(cubeLen, cubeOrigin, shapeTrans, cubeInertia, cubeMass, cubeIndex);
+//               rigid_bodies_vector.push_back(cube);
+//           } 
+//        }
+//    }
+//}
+//
+//void get_spheres(std::vector<VBF::RigidBody*>& sphere_vector){
+//    
+//    double sphereRad = 1.0;
+//    double sphereMass = 1.0;
+//    size_t sphereIndex = 44;
+//    btVector3 sphereInertia = btVector3(0.0, 0.0, 0.0);
+//    btTransform shapeTrans;
+//    shapeTrans.setIdentity();
+//    size_t array_size = 5;
+//       for(size_t k=0; k<array_size; ++k){ 
+//           for (size_t i = 0; i < array_size; ++i) {
+//               for (size_t j = 0; j < array_size; ++j) {
+//                   
+//                   btVector3 sphereOrigin = btVector3(2.0*i, 20+2.0*k, 2.0*j);
+//                   sphereIndex += j + array_size*i + array_size*k;
+//                   VBF::Sphere *sph = new VBF::Sphere(sphereRad, sphereOrigin, shapeTrans, sphereInertia, sphereMass, sphereIndex);
+//                   sphere_vector.push_back(sph);
+//               } 
+//            }
+//        }
+//
+//}
+//
+//
+////this method adds a reference sphere at the axis of  the vbf component for visual check and 
+////debuggging 
+//void get_sphere(std::vector<VBF::RigidBody*>& sphere_vector, btVector3 &meshAxis, const double scale){
+//    
+//    double sphereRad = 1.0;
+//    size_t sphereIndex = 44;
+//    meshAxis[0] *= scale; meshAxis[1] *= scale; meshAxis[2] *= scale;
+//    btVector3 sphereInertia = btVector3(0.0, 0.0, 0.0);
+//    btTransform shapeTrans;
+//    shapeTrans.setIdentity();
+//    VBF::Sphere *sph = new VBF::Sphere(sphereRad, meshAxis, shapeTrans, sphereInertia, 0.0, sphereIndex);
+//    sphere_vector.push_back(sph);
+//
+//}
 
 int main(int argc, char *argv[]){
 
     std::cout << "attempt to run hello world like program using modern c++ and with GUI debugDraw\n";
-
-    //read input data from the input file
-    std::string inputFileName("input.txt");    
-    VBF::InitializeSim init(inputFileName);
 
     //create a placeholder for rigid boides
     std::vector<VBF::RigidBody*> rigid_bodies;
@@ -118,25 +107,10 @@ int main(int argc, char *argv[]){
     btVector3 groundOrigin = get_rigid_body_position(ground);
     std::cout << groundOrigin[0] << " " << groundOrigin[1] << " " << groundOrigin[2] << "\n";
     //import the stl file
-    double scale{0.1}, mass{0.0};
-    bool part1Kin{true}, part2Kin{false};
-    std::string fileName("MeshFiles/StufeFein150x30x200.stl");
-    std::string file2{"MeshFiles/Zylinder1_7x1_0.stl"};
-    btVector3 meshOrigin{btVector3(0.0, 0.0, 00.0)} ;
-    VBF::ImportSTLSetup* stl_body = new VBF::ImportSTLSetup(fileName, scale, mass, part1Kin, meshOrigin);
-
-    rigid_bodies.push_back(stl_body->get_vbf_rbody());
-    VBF::ImportSTLSetup* stl_body2 = new VBF::ImportSTLSetup(file2, 10*scale, 0.1, part2Kin);
-    rigid_bodies.push_back(stl_body2->get_vbf_rbody());
-
     //create world for vbf simulation
     VBF::World* vbf_world = new VBF::World();
     vbf_world->intialize_new_world();
     
-    btVector3 meshAxis{btVector3(73.97719, 0.0, 132.03666)};
-    get_sphere(rigid_bodies, meshAxis, scale); //this is just a reference body
-    
-
     //CommonPhysics phy(vbf_world);
     VBF::CommonPhysics phy(vbf_world, ground, rigid_bodies);
     phy.initPhysics();
@@ -152,37 +126,23 @@ int main(int argc, char *argv[]){
 
     btClock timer;
     unsigned long prevTime = timer.getTimeMicroseconds();
-    btVector3 position;
-    VBF::RigidBody* vbf_meshRbody = stl_body->get_vbf_rbody();
-    btVector3 axis{btVector3(7.397719, 0.25, 13.203666)};
 
-    double Vy{};
-    auto velFun = [] (double time, double amplitude)->double{double pi{3.14159};
-                                  return amplitude*sin(4*pi*time);};
 
     do{
          unsigned long currTime = timer.getTimeMicroseconds();
          if(!vis_bridge.isIdle()){
              phy.stepSimulation((currTime - prevTime)/1000.);
          }
-         Vy = velFun(currTime, 2);
-         btVector3 linVel{btVector3(0.0, Vy, 0.0)};
-         vbf_meshRbody->set_linear_vel(axis, linVel);
          prevTime = currTime;
          vbf_window->start_rendering();
          vis_bridge.renderme();
          phy.debugDraw(2);
          vbf_window->end_rendering();
 
-         btVector3 position1 = get_rigid_body_position(stl_body->get_vbf_rbody());
-         std::cout << "Time:" <<currTime <<" s, Vy:" <<  Vy <<  " x:" << position1[0] << " y:" << position1[1] << " z:" << position1[2] << "\n";
-
         }while(!vbf_window->requested_exit());
         
     vbf_window->close_window();
     //stl_body->~ImportSTLSetup();
-    ground->~RigidBody();
-    init.~InitializeSim();
     //delete window;
    
 return 0;
