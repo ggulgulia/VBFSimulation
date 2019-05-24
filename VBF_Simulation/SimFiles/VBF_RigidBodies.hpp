cf1f367a835b73927b5e4361b059af78831d22be
gajendra <gajendra.gulgulia@tum.de>
2019-05-24 10:35:04 +0200

    Static Cube
    Initial code refactoring to get the static cube inheriting from base
    class Rigid Body.
    
    TODO: create a super class Static Body and inherit the Static Cube and
    sphere from this super class. Repeat for all objects of type Kinematic
    and Dynamic body

--- VBF_Simulation/SimFiles/VBF_RigidBodies.hpp
+++ VBF_Simulation/SimFiles/VBF_RigidBodies.hpp
@@ -16,21 +16,11 @@ namespace VBF{
             std::string m_name;
             CollShape* m_shape;
             btVector3 m_origin;
-            btTransform m_shapeTransform;
-            double m_mass;
-            btVector3 m_inertia;
-            bool m_isKinematic;
             size_t m_index; //might be helpful for book keeping 
                             //when there are many rigid bodies
+            MotionState *m_motionState;
             btRigidBody *m_rbody;
 
-            //dynamic properties of the rigid body
-            //they go in rbconstrinfo
-            double m_frictionCoeff;
-            double m_rollingFriction;
-            double m_restitutionCoeff;
-            double m_linearDamping;
-            double m_angularDamping;
         public:
 
             //default constructor
@@ -39,31 +29,24 @@ namespace VBF{
             //or don't permit call to empty constructor
             explicit RigidBody();
             //user constructor
-            explicit RigidBody(std::string name, CollShape* shape, btVector3 origin,
-                               btTransform shapeTransform, double mass, 
-                               btVector3 inertia, bool isKinematic=false,
-                               double linFriction=0.5, double rollingFriction=0.5, 
-                               double restitution=0.2, double linDamping=0.0, 
-                               double angularDamping=0.0, size_t index=0);
+            explicit RigidBody(std::string name, CollShape* shape, 
+                               btVector3 origin, size_t index=0);
             
             //copy Constructor
-            RigidBody(const RigidBody& vbf_rb);
-            
-            //constructor that simply creates a copy of RigidBody 
-            //by transforming its position (and orientation)   
-            RigidBody(const RigidBody& rbody, btVector3 origin);
+            RigidBody(const RigidBody& vbf_rb)= delete;
             
             //destructor
             virtual ~RigidBody();
             
             //helper functions
+            MotionState* get_motion_state(){return m_motionState;}
+            void set_rbody(btRigidBody* rbody){m_rbody = rbody;}
             virtual btRigidBody* get_rbody() const;
             virtual btRigidBody* get_rbody();
             virtual std::string get_name()   const;
             virtual CollShape* get_shape()   const;
+            virtual CollShape* get_shape();
             virtual btVector3 get_origin()   const;
-            virtual double get_mass()        const;
-            virtual btVector3 get_inertia()  const;
             virtual size_t get_index()       const;
             virtual void set_gravity(const btVector3 gravity= btVector3(0.0,-9.81, 0.0));
             virtual btVector3 get_cog_position();
