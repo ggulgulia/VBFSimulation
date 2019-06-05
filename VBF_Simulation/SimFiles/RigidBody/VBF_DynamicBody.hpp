92466c1c0a186a6086c8d6a5f4a8445e6eddde06
gajendra <gajendra.gulgulia@tum.de>
2019-06-05 11:40:06 +0200

    Static Mesh interfact working
        VBF_StaticMesh.hpp and implementation file included in set up
    
        Basic Features in Static Mesh working at the moment
    
        TODO:
        1. wirte unit tests and check the individual methods
        2. test the main.cpp with an imported static mesh
        3. wirte DynamicBody interface for VBF Simulations

---
+++ VBF_Simulation/SimFiles/RigidBody/VBF_DynamicBody.hpp
@@ -0,0 +1,56 @@
+#ifndef VBF_DYNAMIC_BODY_H
+#define VBF_DYNAMIC_BODY_H 
+
+#include "VBF_RigidBodies.hpp"
+
+namespace VBF{
+
+    class DynamicBody : public RigidBody{
+
+        private:
+            double m_mass;
+            btVector3 m_inertia;
+                            //when there are many rigid bodies
+
+            //dynamic properties of the rigid body
+            //they go in rbconstrinfo
+            double m_frictionCoeff;
+            double m_rollingFriction;
+            double m_restitutionCoeff;
+            double m_linearDamping;
+            double m_angularDamping;
+        public:
+
+            //default constructor
+            //note right now call to this constructor
+            //creates hanging pointers, either resolve this
+            //or don't permit call to empty constructor
+            explicit DynamicBody();
+            //user constructor
+            explicit DynamicBody(std::string name, CollShape* shape, btVector3 origin,
+                               btTransform shapeTransform, double mass, 
+                               btVector3 inertia, bool isKinematic=false,
+                               double linFriction=0.5, double rollingFriction=0.5, 
+                               double restitution=0.2, double linDamping=0.0, 
+                               double angularDamping=0.0, size_t index=0);
+            
+            //preventing implicit copy Constructor
+            DynamicBody(const RigidBody& vbf_rb) = delete;
+            
+            //destructor
+            virtual ~DynamicBody();
+            
+            //helper functions
+            virtual btRigidBody* get_rbody() const;
+            virtual btRigidBody* get_rbody();
+            virtual double get_mass()        const;
+            virtual btVector3 get_inertia()  const;
+            virtual void set_gravity(const btVector3 gravity= btVector3(0.0,-9.81, 0.0));
+            virtual btVector3 get_position();
+            virtual void set_linear_vel(const btVector3& pos, const btVector3& linVel);
+            //virtual void set_angular_vel(const btVector3& axis, const btVector3& angVel);
+    };
+}//end of name space
+
+#endif /* ifndef VBF_RIGID_BODIES_H */
+
---
+++ VBF_Simulation/SimFiles/RigidBody/VBF_StaticMesh.hpp
@@ -0,0 +1,31 @@
+#ifndef STATIC_MESH_BODY_H
+#define STATIC_MESH_BODY_H
+
+#include <bullet/BulletCollision/Gimpact/btGImpactShape.h>
+#include <bullet/BulletCollision/Gimpact/btGImpactCollisionAlgorithm.h>
+#include <OpenGLWindow/GLInstanceGraphicsShape.h>
+//#include "../VBF_World.hpp"
+#include <VBF_StaticBody.hpp>
+
+#include <LoadMeshFromSTL.hpp>
+
+namespace VBF{
+
+    class StaticMeshBody{
+    
+        private:
+            std::string m_filename;
+            double m_scale;
+            VBF_MeshShape*  m_mesh;
+            StaticBody* m_VBF_staticBody;
+        public:
+            StaticMeshBody(const std::string &fileName, double scale=0.1, btVector3 origin = btVector3(0.0, 0.0, 0.0),
+                             size_t index=500);
+            virtual ~StaticMeshBody();
+            StaticBody* get_vbf_rbody() ;
+            virtual VBF_MeshShape* get_mesh() const noexcept;
+            virtual std::string get_file_name() const noexcept;
+            virtual btVector3 get_mesh_origin() const noexcept;
+    };
+}
+#endif //STATIC_MESH_BODY_H
