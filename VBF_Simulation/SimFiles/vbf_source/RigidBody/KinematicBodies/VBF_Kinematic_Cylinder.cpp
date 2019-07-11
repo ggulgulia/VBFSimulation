da0185680e645d581df1696e779b62dd2b7c46df
gajendra <gajendra.gulgulia@tum.de>
2019-07-11 17:09:07 +0200

    Rigid body cylinders radius and height + documentation
    Cylinders can be constructed by passing the parameters
    radius, height and origin of the cylinder directly now
    for all theree types of VBF::RigidBody
    
    Additionally the new cylinder classes have now been documented

--- VBF_Simulation/SimFiles/vbf_source/RigidBody/KinematicBodies/VBF_Kinematic_Cylinder.cpp
+++ VBF_Simulation/SimFiles/vbf_source/RigidBody/KinematicBodies/VBF_Kinematic_Cylinder.cpp
@@ -1,23 +1,25 @@
 #include <VBF_Kinematic_Cylinder.hpp>
 
-VBF::Kinematic_Cylinder::Kinematic_Cylinder(): KinematicBody("Default Kinematic Cube", 
-                          new btBoxShape(btVector3(10.0, 10.0, 10.0)),
+
+VBF::Kinematic_Cylinder::Kinematic_Cylinder(): KinematicBody("Default Kinematic Cylinder", 
+                          new btCylinderShape(btVector3(0.5, 1.0, 0.5)),
                           btVector3(0.0,0.0, 0.0), 23), 
-            m_length(10.0){
+            m_radius(0.5), m_height(2.0){
                             //empty constructor body
             }
 
 //Kinematic_Cylinder Class Definitions
-VBF::Kinematic_Cylinder::Kinematic_Cylinder(double length, btVector3 origin, size_t index):
+VBF::Kinematic_Cylinder::Kinematic_Cylinder(double radius, double height, btVector3 origin, size_t index):
                                     KinematicBody("Kinematic_Cylinder",
-                                    new btBoxShape(btVector3(length, length, length)),
+                                    new btBoxShape(btVector3(radius, 0.5*height, radius)),
                                     origin, index),
-                                    m_length(length){
+                                    m_radius(radius), m_height(height){
                         
                 //empty constructor body
                }
 
 //destructor
 VBF::Kinematic_Cylinder::~Kinematic_Cylinder(){};
-double VBF::Kinematic_Cylinder::get_length()const noexcept {return m_length;}
+double VBF::Kinematic_Cylinder::get_cylinder_radius()const noexcept {return m_radius;}
+double VBF::Kinematic_Cylinder::get_cylinder_height()const noexcept {return m_height;}
 std::string VBF::Kinematic_Cylinder::get_name() const noexcept { return "Kinematic_Cylinder"; }
